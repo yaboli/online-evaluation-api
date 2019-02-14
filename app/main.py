b0e9d5b4eb0c0c3457be40a503c48714ba5de3b2
Yabo Li <liyaboleo@hotmail.com>
2019-02-14 15:15:44 +0800

    DEV: Added SQL handing class and methods in main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,40 @@
 import sys
-
-from flask import Flask
+import logging
+from flask import Flask, request
+from sql_manager import SQLManager
+import time
+import datetime
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+
+# 全域变量，负责用户信息与标注数据管理
+sql_manager = SQLManager()
+
+
+def build_sql_dict(user_id, text):
+    return {"user_id": user_id,
+            "time": datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'),
+            "text": text}
+
+
+# 插入新标注的数据
+@app.route("/insert-new-data", methods=['POST'])
+def insert_new_data():
+
+    request_json = request.json
+    user_id = request_json["user_id"]
+    text = request_json["text"]
+
+    # TODO: 获取并解析标签
+
+    dic = build_sql_dict(user_id, text)
+    mark = sql_manager.insert_new_data(dic)
+
+    if mark == 1:
+        return "Data inserted successfully", 200
+    else:
+        return "An error occurred during data insertion", 500
 
 
 @app.route("/")
@@ -12,6 +44,22 @@ def hello():
         version
     )
     return message
+
+
+# HTTP Errors handlers
+@app.errorhandler(404)
+def url_error(e):
+    return """
+    Wrong URL!
+    <pre>{}</pre>""".format(e), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
 
 
 if __name__ == "__main__":
